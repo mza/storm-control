6c30d2677177bb9af1131c5b81adf5412beacbb9
Matt Wood <matt.wood@sanger.ac.uk>
2008-11-09 17:14:12 +0000

    Added fading background warning for failed builds.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,18 +6,31 @@ jQuery(window).bind("load", function() {
 
  	var container = jQuery("div#slider");
 	var panelWidth = container.find("div.panel").width();
-	var panelCount = container.find("div.panel").size();
+	var panels = container.find("div.panel");
+	var panelCount = panels.size();
+	var statusValues = container.find("input.build_status");
+	var statusLabels = container.find("div.build_status");
+	var body = container.parent().parent();
 	var panel = 0;
-  
+
 	if (location.hash && parseInt(location.hash.slice(1)) <= panelCount) {
 		panel = parseInt(location.hash.slice(1));
 	};
 	
-  	container.everyTime(3000,function(i) {
+  	container.everyTime(5000,function(i) {
 		var nextPanel = panel + 1;
 		if (nextPanel >= panelCount) {
 			nextPanel = 0
 		}
+		
+		if (statusValues.get(nextPanel).value == "1") {
+/*			statusLabels.get(nextPanel).innerHTML = "OH YEAH - PASS";*/
+			body.animate({ backgroundColor: "#282828" }, 'slow');
+		} else {
+/*			statusLabels.get(nextPanel).innerHTML = "OH NO - FAIL";	*/
+			body.animate({ backgroundColor: "#67040C" }, 'slow');
+		}
+		
 		var change = - (panelWidth * (nextPanel));
 	 	$("div.panelContainer").animate({ left: change });
 		panel = nextPanel;
