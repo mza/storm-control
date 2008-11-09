d21e246073b09d41ab2ab8e60233382b178f293d
Matt Wood <matt.wood@sanger.ac.uk>
2008-11-09 17:41:01 +0000

    Added location to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @cruise_control = CruiseControl.new
     @refresh = setting_for :refresh
+    @location = setting_for :location
   end
   
 end
